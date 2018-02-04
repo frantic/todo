aa5b90c4ef21a6660a12f4338a4e37153ea224b9
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-02-04 13:11:57 -0800

    Move reducer to its own module

--- App.js
+++ App.js
@@ -5,42 +5,10 @@ import { Provider } from 'react-redux';
 import devtools from 'remote-redux-devtools';
 
 import TodoScreen from './App/Screens/TodoScreen';
-import { RESTORE_STATE, ADD_TODO, TOGGLE_TODO, restoreState } from './App/Store/Actions';
+import { restoreState } from './App/Store/Actions';
+import { reducer } from './App/Store/Reducer';
 
-const initialStoreState = {
-  todos: [
-    { id: 1, title: 'Build TODO app in under 1 hour', done: false },
-    { id: 2, title: 'Prepare slides', done: false },
-    { id: 3, title: 'Eat some cake', done: true },
-  ],
-};
-
-function reducer(state, action) {
-  if (action.type === RESTORE_STATE) {
-    return action.state;
-  }
-  if (action.type === ADD_TODO) {
-    return {
-      todos: [
-        { id: action.id, title: action.title, done: false },
-        ...state.todos,
-      ],
-    }
-  }
-  if (action.type === TOGGLE_TODO) {
-    const updatedTodos = state.todos.map(
-      todo => todo.id === action.id
-        ? {...todo, done: !todo.done}
-        : todo
-    );
-    return {
-      todos: updatedTodos,
-    };
-  }
-  return state;
-}
-
-const store = createStore(reducer, initialStoreState, devtools());
+const store = createStore(reducer, devtools());
 
 // Primitive version of redux-persist
 AsyncStorage.getItem('state').then(json => {
