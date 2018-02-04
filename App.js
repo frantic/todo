fb48ff648ac75cca8c092f0ae67f706a5a3fa48b
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-02-03 17:02:57 -0800

    Add simple redux store

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import TodoScreen from './App/Screens/TodoScreen';
+import { todos } from './App/Store/TodoReducer';
+
+const store = createStore(todos);
 
 export default class App extends React.Component {
   render() {
-    return <TodoScreen />;
+    return (
+      <Provider store={store}>
+        <TodoScreen />
+      </Provider>
+    );
   }
 }
---
+++ App/Store/TodoReducer.js
@@ -0,0 +1,5 @@
+const initialState = [];
+
+export function todos(state = initialState, action) {
+  return state;
+}
