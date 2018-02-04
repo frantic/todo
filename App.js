d9efb368cf0dda7473ab4611b1b8ab0f872f4f28
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-02-03 16:09:01 -0800

    Simple stateful checkbox component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Checkbox from './App/Components/Checkbox';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Checkbox />
       </View>
     );
   }
---
+++ App/Components/Checkbox.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default class Checkbox extends React.Component {
+  state = {
+    isChecked: false,
+  };
+
+  render() {
+    const isChecked = this.state.isChecked;
+    return (
+      <TouchableOpacity onPress={() => this.setState({isChecked: !isChecked})}>
+        <View style={styles.outer}>
+          {isChecked ? <View style={styles.inner} /> : null}
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const SIZE = 32;
+const INNER_SIZE = 24;
+
+const styles = StyleSheet.create({
+  outer: {
+    width: SIZE,
+    height: SIZE,
+    borderRadius: SIZE / 2,
+    borderColor: '#4EAFF2',
+    borderWidth: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inner: {
+    width: INNER_SIZE,
+    height: INNER_SIZE,
+    borderRadius: INNER_SIZE / 2,
+    backgroundColor: '#4EAFF2',
+  },
+});
