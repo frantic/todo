79e097e86363d2d07d2eb0d545c4bea67d304aae
Alex Kotliarskyi <alex.frantic@gmail.com>
2018-02-04 11:35:36 -0800

    Create a simple Redux store

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const initialStoreState = {
+  todos: [
+    { id: 1, title: 'Build TODO app in under 1 hour', done: false },
+    { id: 2, title: 'Prepare slides', done: false },
+    { id: 3, title: 'Eat some cake', done: true },
+  ],
+};
+
+function reducer(state) {
+  // We'll talk about what this does later
+  return state;
+}
+
+const store = createStore(reducer, initialStoreState);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+        </View>
+      </Provider>
     );
   }
 }
